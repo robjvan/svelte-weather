855f4a20dd61f3b55e8d9e119ad856f7fa6834d2
Rob Vandelinder <robjvan@gmail.com>
2022-10-28 18:15:05 -0230

    Added about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,15 @@
+<script></script>
+
+<svelte:head>
+  <title>About MiniWeather</title>
+</svelte:head>
+
+<h1>About MiniWeather</h1>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo earum esse minus soluta. Facere reprehenderit deserunt deleniti atque ullam velit maxime itaque. Deserunt, laudantium quos dignissimos ea doloribus at esse.</p>
+
+<style>
+ h1 {
+  /* color: red; */
+ }
+</style>
