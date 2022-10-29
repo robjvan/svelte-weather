ed7dc1dca9848a02f5b17e6873b3b542fd7a6779
Rob Vandelinder <robjvan@gmail.com>
2022-10-29 00:21:43 -0230

    Refactored Temp widget

---
+++ src/components/temp.widget.svelte
@@ -0,0 +1,22 @@
+<script lang=ts>
+	import { weatherStore } from '../stores/weather-store';
+	import { settingsStore } from '../stores/settings-store';
+	import type { WeatherData } from 'src/interfaces/weather-data.interface';
+  import { get } from 'svelte/store';
+	import { subscribe } from 'svelte/internal';
+	</script>
+
+	<img src={$weatherStore.conditionIconUrl} alt="Conditions icon" />
+  <br>
+  <p>Feels like</p>
+  {#if $settingsStore.useMetric}
+  <p>{$weatherStore.feelsLikeC} C</p>
+  {:else}
+  <p>{$weatherStore.feelsLikeF} F</p>
+  {/if}
+<style>
+  img {
+    height: 200px; 
+    margin: auto;
+  }
+</style>
