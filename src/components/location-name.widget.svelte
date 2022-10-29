a2078f82781456b858378c1329f01dcb4c4b9b9f
Rob Vandelinder <robjvan@gmail.com>
2022-10-29 00:21:12 -0230

    Refactored LocationName widget

---
+++ src/components/location-name.widget.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { weatherStore } from '../stores/weather-store';
+	
+</script>
+
+<h1 class="text-3xl text-center pt-4">{$weatherStore.locationName}, {$weatherStore.locationRegion}</h1>
+		
