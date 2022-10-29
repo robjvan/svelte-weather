0f360898779101d05eded19f9844b1585dc99f3f
Rob Vandelinder <robjvan@gmail.com>
2022-10-29 00:20:47 -0230

    Added loading spinner

---
+++ src/components/progress-spinner.svelte
@@ -0,0 +1,14 @@
+<div class="ispinner gray animating">
+	<div class="ispinner-blade" />
+	<div class="ispinner-blade" />
+	<div class="ispinner-blade" />
+	<div class="ispinner-blade" />
+	<div class="ispinner-blade" />
+	<div class="ispinner-blade" />
+	<div class="ispinner-blade" />
+	<div class="ispinner-blade" />
+	<div class="ispinner-blade" />
+	<div class="ispinner-blade" />
+	<div class="ispinner-blade" />
+	<div class="ispinner-blade" />
+</div>
