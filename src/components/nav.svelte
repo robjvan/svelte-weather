5a9b840a719ae7d45b9e6249a7abac97a4bb7b0b
Rob Vandelinder <robjvan@gmail.com>
2022-10-29 00:20:59 -0230

    Added nav bar

---
+++ src/components/nav.svelte
@@ -0,0 +1,28 @@
+<script>
+	import FaHome from 'svelte-icons/fa/FaHome.svelte';
+	import FaInfo from 'svelte-icons/fa/FaInfo.svelte';
+	import { settingsStore } from '../stores/settings-store';
+	import Switch from './switch.svelte';
+</script>
+
+<nav class="flex justify-between w-full">
+	<a href="/"><div class="icon"><FaHome /></div></a>
+	<div class="icon mx-auto" style="width: 200px;">
+		<center>
+			<Switch bind:value={$settingsStore.useMetric} />
+		</center>
+		<p style="text-align: center;">Toggle metric</p>
+		</div>
+	<a class="mx-4 text-lg" href="/about"><div class="icon"><FaInfo /></div></a>
+</nav>
+
+<style>
+	.icon {
+		width: 32px;
+		height: 32px;
+		color: #888;
+	}
+	nav {
+		margin-bottom: 16px;
+	}
+</style>
